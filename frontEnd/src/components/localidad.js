6fc8ece1679af36dff97800c245e90b81825f035
Laura <l.castro12@uniandes.edu.co>
2017-03-05 16:51:08 -0500

    Cambios componente localidad

--- frontEnd/src/components/localidad.js
+++ frontEnd/src/components/localidad.js
@@ -4,7 +4,17 @@ export default class Localidad extends Component {
 
   render() {
     return (
-      <div >Localidad</div>
+    	<div className="container">
+    	<div className="container">
+    	<h2 className="primero">¿Qué deseas hacer?</h2>
+    	<div className="btn-group"  data-toggle="buttons-radio">
+    	<button className="btn">Buscar Equipo</button>
+    	<button className="btn">Reclutar Equipo</button>
+    	</div>
+    	</div>
+      <h2 className="container">Localidades</h2>
+      <p className="escoge">Escoge la localidad en la que te gustaría jugar</p>
+      </div>
     );
   }
 }
