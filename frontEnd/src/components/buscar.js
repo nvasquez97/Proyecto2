3d8afddd95920f968aeaed346f7aab1671fb5dfb
Nicolas Vasquez <n.vasquez10@uniandes.edu.co>
2017-03-04 21:46:36 -0500

    Backend Mongo.. ya casi

--- frontEnd/src/components/buscar.js
+++ frontEnd/src/components/buscar.js
@@ -1,10 +1,34 @@
 import React, { Component } from 'react';
-
+import axios from 'axios'
+const URL="localhost:27017";
 export default class Buscar extends Component {
 
+
+  constructor(props)
+  {
+    super(props);
+    this.state={
+      reservas:[],
+      id:''
+    }
+  }
   render() {
     return (
       <div >Busca tus vainas</div>
     );
+  }
+
+  obtenerReservas()
+  {
+    axios.get(URL+"/")
+  }
+
+  obtenerEstudiantes() {
+    axios.get(ROOT_URL+ "/estudiantes")
+    .then(response => {
+      this.setState({
+        estudiantes: response.data
+      })
+    })
   }
 }
