bb76d5d7adc3eaad585af20009dd8605dd9d5ea7
Laura <l.castro12@uniandes.edu.co>
2017-03-06 01:46:05 -0500

    backend crud

--- backEnd/models/localidad.js
+++ backEnd/models/localidad.js
@@ -16,3 +16,61 @@ exports.list= function(req, res) {
         });
     });
 };
+
+exports.get = function(req, res) {
+    var id = req.params.id;
+    console.log('Retrieving localidad: ' + id);
+    db.collection('localidad', function(err, collection) {
+        collection.findOne({'_id':new BSON.ObjectID(id)}, function(err, item) {
+            res.send(item);
+        });
+    });
+};
+
+exports.add = function(req, res) {
+    var localidad = req.body;
+    console.log('Adding localidad: ' + JSON.stringify(localidad));
+    db.collection('localidad', function(err, collection) {
+        collection.insert(localidad, {safe:true}, function(err, result) {
+            if (err) {
+                res.send({'error':'An error has occurred'});
+            } else {
+                console.log('Success: ' + JSON.stringify(result[0]));
+                res.send(result[0]);
+            }
+        });
+    });
+}
+
+exports.update= function(req, res) {
+    var id = req.params.id;
+    var localidad = req.body;
+    console.log('Updating localidad: ' + id);
+    console.log(JSON.stringify(localidad));
+    db.collection('localidad', function(err, collection) {
+        collection.update({'_id':new BSON.ObjectID(id)}, localidad, {safe:true}, function(err, result) {
+            if (err) {
+                console.log('Error updating localidad: ' + err);
+                res.send({'error':'An error has occurred'});
+            } else {
+                console.log('' + result + ' document(s) updated');
+                res.send(localidad);
+            }
+        });
+    });
+}
+
+exports.delete = function(req, res) {
+    var id = req.params.id;
+    console.log('Deleting localidad: ' + id);
+    db.collection('localidad', function(err, collection) {
+        collection.remove({'_id':new BSON.ObjectID(id)}, {safe:true}, function(err, result) {
+            if (err) {
+                res.send({'error':'An error has occurred - ' + err});
+            } else {
+                console.log('' + result + ' document(s) deleted');
+                res.send(req.body);
+            }
+        });
+    });
+}
--- backEnd/models/reserva.js
+++ backEnd/models/reserva.js
@@ -16,3 +16,61 @@ exports.list= function(req, res) {
         });
     });
 };
+
+exports.get = function(req, res) {
+    var id = req.params.id;
+    console.log('Retrieving reserva: ' + id);
+    db.collection('reserva', function(err, collection) {
+        collection.findOne({'_id':new BSON.ObjectID(id)}, function(err, item) {
+            res.send(item);
+        });
+    });
+};
+
+exports.add = function(req, res) {
+    var reserva = req.body;
+    console.log('Adding reserva: ' + JSON.stringify(reserva));
+    db.collection('reserva', function(err, collection) {
+        collection.insert(reserva, {safe:true}, function(err, result) {
+            if (err) {
+                res.send({'error':'An error has occurred'});
+            } else {
+                console.log('Success: ' + JSON.stringify(result[0]));
+                res.send(result[0]);
+            }
+        });
+    });
+}
+
+exports.update= function(req, res) {
+    var id = req.params.id;
+    var reserva = req.body;
+    console.log('Updating reserva: ' + id);
+    console.log(JSON.stringify(reserva));
+    db.collection('reserva', function(err, collection) {
+        collection.update({'_id':new BSON.ObjectID(id)}, reserva, {safe:true}, function(err, result) {
+            if (err) {
+                console.log('Error updating reserva: ' + err);
+                res.send({'error':'An error has occurred'});
+            } else {
+                console.log('' + result + ' document(s) updated');
+                res.send(reserva);
+            }
+        });
+    });
+}
+
+exports.delete = function(req, res) {
+    var id = req.params.id;
+    console.log('Deleting reserva: ' + id);
+    db.collection('reserva', function(err, collection) {
+        collection.remove({'_id':new BSON.ObjectID(id)}, {safe:true}, function(err, result) {
+            if (err) {
+                res.send({'error':'An error has occurred - ' + err});
+            } else {
+                console.log('' + result + ' document(s) deleted');
+                res.send(req.body);
+            }
+        });
+    });
+}
