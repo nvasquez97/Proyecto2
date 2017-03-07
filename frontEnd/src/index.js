d0d179e5439332d9636a4bac1ea45b81dd65cfe3
Jose Gabriel Tamura <jg.tamura10@uniandes.edu.co>
2017-03-07 01:08:23 -0500

    padre para los componentes

--- frontEnd/src/index.js
+++ frontEnd/src/index.js
@@ -6,7 +6,8 @@ import FutbolYa from './components/futbolYa';
 import Localidad from './components/localidad';
 import Buscar from './components/buscar';
 import InfoReservas from './components/infoReservas';
-
+/*Aquí tal vez podrian tener un padre para todos estos componentes, para dar mas orden y entender en la pagina como se van a renderizar las cosas*/
+/*Asi como lo tienen no es tan claro que se renderiza en donde...*/
 ReactDOM.render(<FutbolYa /> , document.querySelector('.futbolYa'));
 ReactDOM.render(<Localidad/> , document.querySelector('.localidad'));
 ReactDOM.render(<Buscar/> , document.querySelector('.buscar'));
