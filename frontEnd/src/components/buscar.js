1dc62bac922579a8a67d0b709ff0e06f1c29dab7
Nicolas Vasquez <n.vasquez10@uniandes.edu.co>
2017-03-07 08:36:31 -0500

    Ultimo commit antes de clase

--- frontEnd/src/components/buscar.js
+++ frontEnd/src/components/buscar.js
@@ -32,7 +32,7 @@
             Busca un partido en tu cancha favorita:
           </h1>
           <div className='row'>
-            <button className="btn btn-default" onClick="this.futbol(5).bind(this)">Fútbol 5</button>
+            <button className="btn btn-default" onClick={this.futbol(5).bind(this)}>Fútbol 5</button>
             <button className="btn btn-default">Fútbol 7</button>
             <button className="btn btn-default">Fútbol 8</button>
             <button className="btn btn-default">Fútbol 11</button>
